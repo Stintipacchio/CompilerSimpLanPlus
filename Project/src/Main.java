4252fa927e0158165d04fe2f7996c3a52b5763ae
Andrea Loretti <andrea.loretti00@gmail.com>
2023-06-28 18:20:17 +0200

    Primo push

---
+++ Project/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
